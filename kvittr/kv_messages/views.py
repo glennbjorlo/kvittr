d2b452d2efd873d6d6bd9638e07950a4312a5c5c
glennbjorlo <g-amb@hotmail.com>
2015-05-15 00:32:12 +0200

    Fixed problem with kveets not being stored in database

--- kvittr/kv_messages/views.py
+++ kvittr/kv_messages/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.utils import timezone
+from django.http import HttpResponse
+from django.template import RequestContext
 
 from kv_messages.models import Message
 
 # Create your views here.
 
 def message_listing(request):
+	if request.method == 'POST':
+		new_message = Message()
+		new_message.uname = request.user
+		new_message.umessage = request.POST.get('comment')
+		new_message.created_datetime = timezone.now()
+		new_message.save()
 	kv_messages = Message.objects.all()
 	context = {'kv_messages': kv_messages}
 	return render(request, 'kv_messages/wall.html', context)
@@ -17,13 +26,7 @@ def message_details(request, message_id):
 	return render(request, 'kv_messages/details.html', context)
 
 
-def create_new_message(request):
-	if request.method == "POST":
-		message = Message()
-		message.uname = request.user
-		message.umessage = request.POST.get('comment')
-		message.created_datetime = timezone.now()
-		message.save()
-	return render(request, 'kv_messages/wall.html')
+#def create_new_message(request):
+	
 	
 	
